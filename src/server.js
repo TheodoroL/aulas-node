1cef85b8473d75d4bc9c297f203da162b8745b54
Programador Pato <74570593+Akashipy201@users.noreply.github.com>
2024-02-04 15:36:10 -0300

    Aprendendo sobre métodos e URL http

--- src/server.js
+++ src/server.js
@@ -1,8 +1,18 @@
 import http from 'node:http'; 
 //primeiro servidor http
 const server =  http.createServer((req, res)=>{
+    //vai pegar o method e a url 
+    const {method, url}= req
+    //verificando o method e a url da rota
+    if(method == "GET" && url =="/users"){
+        return res.end("lista de usuários"); 
+    }
+    if(method == "POST" && url== "/users"){
+        return res.end("Criação de usuários"); 
+    }
+
     //vai escrever ola mundo
-    return res.end("hello wo12231r2d")
+    return res.end("hello word"); 
 })
 //porta do servidor 
 server.listen(3333)
