d470177da071f8eacc51ddac765c89809a4a474d
Programador Pato <74570593+Akashipy201@users.noreply.github.com>
2024-02-05 10:42:36 -0300

    Criando stream de leitura

--- streams/fundamentals.js
+++ streams/fundamentals.js
@@ -6,3 +6,43 @@
 //      - 10mb/s para ler e escrever no banco de dadosum arquivo de 1gb vai demorar mais ou menos 100s
 //      - mas em 10mb/s o back end já leu mais o menos 10k de linhas, então por que não serir essa quantidade ? 
 // Esse é o conceito de Streams
+
+
+//Criando a Stream de leitura
+
+
+// process.stdin
+// .pipe(process.stdout) // tudo que eu estou recebendo como entrada, estou caminhando(com o pipe) para saida
+
+import {Readable}from'stream'; 
+
+//criando Readable do zero
+class OneToHundredStream extends Readable{
+    index = 1;
+    _read(){
+        const i = this.index++; 
+        
+        // if(i > 100){
+        //     this.push(null); 
+        // }else{
+        //     // vai converter para buffer
+        //     const buffer = Buffer.from(String(i))
+        //     this.push(buffer); 
+        // }
+        
+        // cada 1s vai adicionar mais um dado
+        setTimeout(()=>{
+        if(i > 100){
+            this.push(null); 
+        }
+        else{
+            // vai converter para buffer
+            const buffer = Buffer.from(String(i))
+            this.push(buffer); 
+        }
+        },1000)
+        
+    }
+}
+
+new OneToHundredStream().pipe(process.stdout)
