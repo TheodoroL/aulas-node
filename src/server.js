317b587505baffed962f1414351923fe5b2a78db
Programador Pato <74570593+Akashipy201@users.noreply.github.com>
2024-02-05 14:05:47 -0300

    Corpo da requisição em JSON (Stream & Buffers)

--- src/server.js
+++ src/server.js
@@ -3,10 +3,21 @@ import http from 'node:http';
 const users = []; 
 
 //primeiro servidor http
-const server =  http.createServer((req, res)=>{
-    //vai pegar o method e a url 
+const server =  http.createServer(async(req, res)=>{
+    const buffer = [];
+    for await(const chunk of req){
+        buffer.push(chunk); 
+    }
+    //vai converter o buffer de uma string para um json
+    try{
+
+        req.body = JSON.parse(Buffer.concat(buffer).toString()); 
+    }catch{
+        req.body = null
+    }
+   
+    
     const {method, url}= req
-    //verificando o method e a url da rota
     if(method == "GET" && url =="/users"){
         return res
         //vaicriar um Cabeçalhos 
@@ -14,12 +25,14 @@ const server =  http.createServer((req, res)=>{
         .end(JSON.stringify(users)); 
     }
     if(method == "POST" && url== "/users"){
+
+        const {nome, email} = req.body; 
         users.push({
-            nome: "Lucas", 
-            email : "teste1234@gmail.com",
+            nome, 
+            email
         })
         // vai enviar que o status é 201, obs : nós retorna sempre quando algo é criado na aplicação do back end
-        return res.writeHead(201).end(); 
+        return res.writeHead(201).end("usuario criado com sucesso"); 
     }
 
     //se não existir rotas criada, vai retonar 404
