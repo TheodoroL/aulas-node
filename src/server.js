0a041684b30e4940a9735ab77a3ee046ce75c960
Programador Pato <74570593+Akashipy201@users.noreply.github.com>
2024-02-06 13:23:10 -0300

    Criando ID único e universal (UUID)

--- src/server.js
+++ src/server.js
@@ -1,10 +1,9 @@
 import http from 'node:http'; 
 import { json } from './middlewares/json.js';
 import { Database } from './database.js';
-//lista de usuários
+import {randomUUID} from 'node:crypto'; 
 const database = new Database(); 
 
-//primeiro servidor http
 const server =  http.createServer(async(req, res)=>{
     const {method, url}= req
 
@@ -13,24 +12,22 @@ const server =  http.createServer(async(req, res)=>{
     if(method == "GET" && url =="/users"){
         const users = database.select("users"); 
         return res
-        //vai criar um Cabeçalhos 
         .end(JSON.stringify(users)); 
     }
     if(method == "POST" && url== "/users"){
 
         const {nome, email} = req.body; 
+        //criando ID unico para nosso clientes
         const users={
+            id : randomUUID(), 
             nome, 
             email
         }
 
         database.insert('users', users); 
-        // vai enviar que o status é 201, obs : nós retorna sempre quando algo é criado na aplicação do back end
         return res.writeHead(201).end("usuario criado com sucesso"); 
     }
 
-    //se não existir rotas criada, vai retonar 404
     return res.writeHead(404).end(); 
 })
-//porta do servidor 
 server.listen(3333)
